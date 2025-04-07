8bff0a72dcdbfb2e8045fa327c145ca4504ee4d6
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-07 14:39:41 +0200

    Fixing navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar  from "../Components/Navbar";
+import  Na  from "../Components/Navbar";
 import { Footer } from "../Components/Footer";
 import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
   return (
     <div>
-      <Navbar />
+      <Na />
       <Outlet />
       <Footer />
     </div>
