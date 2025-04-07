d060700f5e56d9eef68cd39b129e5b1d694027c0
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-07 15:09:58 +0200

    removing tranding

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,14 +2,13 @@ import { Categories } from "../components/Categories";
 import { Features } from "../components/Features";
 import { FlashSales } from "../components/FlashSales";
 import { Hero } from "../components/Hero";
-import Tranding from "../Components/Tranding";
 
 export const Homepage = () => {
   return (
     <div>
       <Hero />
       <Features />
-      <Tranding />
+      {/* <Tranding /> */}
       <FlashSales />
       <Categories />
     </div>
